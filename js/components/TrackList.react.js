979e6c299813a3f8bc8a84a86938ac0364dc6e29
Jake Champion <jakechampion.jake2@googlemail.com>
2015-05-25 23:22:47 +0100

    Use lodash modules for compact build

--- js/components/TrackList.react.js
+++ js/components/TrackList.react.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import _ from 'lodash'
+import map from 'lodash/collection/map'
 import Track from './Track.react'
 import PureComponent from './PureComponent.react'
 // import PropCheckMixin from '../mixins/PropCheckMixin'
 
 export default class TrackList extends PureComponent {
   render () {
-    const tracks = _.map(this.props.tracks, function (track) { return track[Object.keys(track)[0]] })
+    const tracks = map(this.props.tracks, function (track) { return track[Object.keys(track)[0]] })
     const playing = true
 
-    const Tracks = _.map(tracks, function (track) {
+    const Tracks = map(tracks, function (track) {
       const file = Object.keys(track._attachments)[0]
       return <Track key={track.rev} rev={track.rev} id={track.id} artist={track.artist} album={track.album} title={track.title} trackNumber={track.number} duration={track.genre} playing={playing} favourite={track.favourite} attachment={file} genre={track.genre}/>
     })
